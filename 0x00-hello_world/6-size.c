9dd2e842be14af19812e4c23dc1af40fc8047d40
ian kariuki <iankariuki118@gmail.com>
2023-09-23 20:44:29 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - a program about grandeur
+ * Return: 0 (success)
+ */
+int main(void)
+{
+ char a;
+ int b;
+ long int l;
+ long long int m;
+ float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+ printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+ printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(m));
+ printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+  return (0);
+}
