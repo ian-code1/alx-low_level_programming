8c5ede307de130deae8872cd8fb4879acd005d3e
ian kariuki <iankariuki118@gmail.com>
2023-09-24 12:30:31 -0700

    1-strncat.cl

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,34 @@
 #include "main.h"
-/**
- *_strcat - concantenates two strings
- *@dest: input value
- *@src: input value
- *return: void
- */
 
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string to which the source will be appended.
+ * @src: The source string to append to the destination.
+ * Return: A pointer to the resulting string (dest).
+ */
 char *_strcat(char *dest, char *src)
 {
-int i;
-int j;
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
-return (dest);
+    int i;
+    int j;
+
+    // Find the end of the dest string
+    i = 0;
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+
+    // Copy characters from src to dest
+    j = 0;
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;  // You were missing a semicolon here
+    }
+
+    // Add a terminating null byte to the end of the combined string
+    dest[i] = '\0';
+
+    return dest;
 }
