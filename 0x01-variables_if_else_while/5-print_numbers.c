51e167d3005db733a6042b0fbd8426a03f521a54
ian kariuki <iankariuki118@gmail.com>
2023-10-13 00:15:14 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - program entry point
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+int i = 0;
+while (i <= 10)
+{
+printf("%d \n", i);
+i++;
+}
+return (0);
+}
