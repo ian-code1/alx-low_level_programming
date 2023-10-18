cf32295ef676de062fe09a3f81cc4f2c2d377974
ian kariuki <iankariuki118@gmail.com>
2023-10-18 03:23:11 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *mul - program to check for multiplication
+ *@a: a variable
+ *@b: the second variable
+ *Return: Always 0
+ */
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
