6d371aa9220cbb9bccfba46dea6d74637fce8413
ian kariuki <iankariuki118@gmail.com>
2023-10-27 02:37:00 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev -  a function that prints a string, in reverse
+ * @s: string
+ *Return: always 0 (success)
+*/
+void print_rev(char *s)
+{
+int longi = 0;
+int o;
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+s--;
+for (o = longi; o > 0; o--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
