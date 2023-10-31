0e983674baa9a72a42ace71fd94db0aaf1fdffd0
ian kariuki <iankariuki118@gmail.com>
2023-10-31 07:59:28 -0700

    ./0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,27 +8,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i;
-    int j;
-
-    // Find the end of the dest string
-    i = 0;
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
-
-    // Copy characters from src to dest
-    j = 0;
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;  // You were missing a semicolon here
-    }
-
-    // Add a terminating null byte to the end of the combined string
-    dest[i] = '\0';
-
-    return dest;
+int i;
+int j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
 }
