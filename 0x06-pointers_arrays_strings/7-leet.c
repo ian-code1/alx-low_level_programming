457db2bf8217be8fe0cf2f52c67e8e14de640045
ian kariuki <iankariuki118@gmail.com>
2023-11-03 03:31:39 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet - encode string to 1337
+ * @n: input value
+ *Return: n value
+ */
+char *leet(char *n)
+{
+int i, j;
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
+for (i = 0; n[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (n[i] == si[j])
+{
+n[i] = s2[i];
+}
+}
+}
+return (n);
+}
