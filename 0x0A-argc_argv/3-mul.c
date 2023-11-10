b5f851c7840452f835ec02fd594ea038307187ca
ian kariuki <iankariuki118@gmail.com>
2023-11-09 21:51:08 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,13 +6,13 @@
  * @argv: the arrray that contains more tha the ..
  * Return: always 0
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-pritnf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
 return (0);
 }
