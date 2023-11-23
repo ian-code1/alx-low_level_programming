93dba17e7f4d8b0a32e9ee7ad80b1f59c2523a05
ian kariuki <iankariuki118@gmail.com>
2023-11-22 19:42:33 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a struct that creates a dog info
+ * @name: the name if the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Description: first struct of the dog clause
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
