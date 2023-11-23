ff477e811664fabedc4732c18a4958a1675be811
ian kariuki <iankariuki118@gmail.com>
2023-11-22 16:25:17 -0800

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+/**
+ * File: 3-function_like_macro.h
+ * @x: input value
+ * Desc: header file that computes absolute value
+ */
+#define ABS(x) (x < 0 ? -(x) : (x))
+#endif
