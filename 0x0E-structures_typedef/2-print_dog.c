9a38ccc35ebf3c93fe2959a418e182ec67f6ee0f
ian kariuki <iankariuki118@gmail.com>
2023-11-26 23:21:35 -0800

    dog.h

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,7 +8,7 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-if (!((*d).name))
+if (!(d->name))
 	printf("name: (nil)\n");
 else
 	printf("Name: %s\n", d->name);
@@ -16,6 +16,6 @@ printf("Age: %f\n", d->age);
 if (!(d->owner))
 	printf("Owner: (nil)\n");
 else
-	printf("Owner: %s\n", (*d).owner);
+	printf("Owner: %s\n", d->owner);
 }
 }
